226c23af036338285704446f8bb82de8f756c4ef
Dmitry Morozov <9984398@gmail.com>
2022-05-14 12:31:55 +0300

    bard.draw : Moved to separate file.

---
+++ Project_TicTacToe/board_draw.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Dmitry Morozov on 14/5/22.
+//
+
+#include <iostream>
+
+//TODO: Move code to separate file next time!
+
+using namespace std;
+
+void drawBoard () {
+    cout << "   |    | \n ";
+    cout << "--|----|-- \n";
+    cout << "   |    | \n ";
+    cout << "--|----|-- \n";
+    cout << "   |    | \n ";
+
+}
+
+int main () {
+    drawBoard ();
+    return 0;
+}
