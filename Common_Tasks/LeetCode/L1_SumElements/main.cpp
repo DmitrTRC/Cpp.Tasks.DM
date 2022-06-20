a33547e7f9e9014ddb3d5574dae7456d0c169f8a
Dev Spain <9984398@gmail.com>
2022-06-21 00:37:27 +0300

    Code Review : Done

--- Common_Tasks/LeetCode/L1_SumElements/main.cpp
+++ Common_Tasks/LeetCode/L1_SumElements/main.cpp
@@ -3,28 +3,49 @@
 //
 #include <iostream>
 #include <vector>
+
+//Conclusion : Great job!
+
 using namespace std;
+
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        vector <int>runningNums;
-        int tmp=0;
-        for (int i=0;i<nums.size();i++){
-            tmp+=nums[i];
-            runningNums.push_back(tmp);
+    vector<int> runningSum (vector<int> &nums) {
+        vector<int> runningNums;
+        int tmp = 0;
+        for (int i = 0; i < nums.size (); i++) {
+            tmp += nums[i];
+            runningNums.push_back (tmp);
         }
-        for (auto i:runningNums)
-           cout<<i<<" ";
-        return runningNums;
-        
+        // Look! You have to use it only for debug purposes! Test will be passed without it! :)
+        // Almost time test crashes if you use it!
+        //Right way to test your code outside the class.
+
+//        for (auto i:runningNums)
+//           cout<<i<<" ";
+
+        return runningNums; //Right way just return runningNums. Ok
+
     }
 };
-int main()
-{
+
+//It's more comfortable to write separate print function for debug purposes.
+void printResult (const vector<int> &result) {
+    for (auto i: result)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
+
+
+int main () {
     Solution run;
-   vector<int>num={1,2,3,4};
-   vector<int>num2={1,1,1,1,1};
-   run.runningSum(num);
-   cout<<"\n";
-   run.runningSum(num2);
+    vector<int> num = {1, 2, 3, 4}; // Ok! But not necessary :)
+    vector<int> num2 = {1, 1, 1, 1, 1};
+    // You can write it shorter:
+//    printResult ({1, 3, 6, 10});
+//    printResult ( {1, 1, 1, 1, 1});
+
+    printResult (run.runningSum (num));//
+    printResult (run.runningSum (num2));
+
 }
