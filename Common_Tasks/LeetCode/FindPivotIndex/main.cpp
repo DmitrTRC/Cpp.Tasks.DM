d6d4a46eceaa7e3e7bc684ffd497690ffa2d33db
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-06-25 21:04:37 +0300

    Update main.cpp

--- Common_Tasks/LeetCode/FindPivotIndex/main.cpp
+++ Common_Tasks/LeetCode/FindPivotIndex/main.cpp
@@ -2,9 +2,28 @@
 // Created by Dmitry Morozov on 21/6/22.
 //
 
-class Solution {
-public:
-    int pivotIndex (vector<int> &nums) {
+class Solution
+{
+  public:
+  int pivotIndex (vector < int >&nums)
+  {
 
-    }
+    int Rs = 0;
+    int Ls = 0;
+    for (int number:nums)
+      {
+	Rs += number;
+      }
+
+    for (int i = nums.size (); i < nums.size (); i++)
+      {
+	Rs -= nums[i];
+	    if (Ls == Rs)
+	        return i;
+    	Ls += nums[i];
+      }
+    return -1;
+  }
 };
+int main{
+}
