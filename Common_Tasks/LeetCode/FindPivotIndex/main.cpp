677955b8bb2483b039815c283769a60b4fc5bad4
Dev Spain <9984398@gmail.com>
2022-06-26 11:14:30 +0300

    Code review : added.

--- Common_Tasks/LeetCode/FindPivotIndex/main.cpp
+++ Common_Tasks/LeetCode/FindPivotIndex/main.cpp
@@ -3,30 +3,32 @@
 //
 #include <iostream>
 
+
 using namespace std;
+
 #include <vector>
-class Solution
-{
-  public:
-  int pivotIndex (vector < int >&nums)
-  {
 
-    int Rs = 0;
-    int Ls = 0;
-    for (int number:nums)
-      {
-	Rs += number;
-      }
 
-    for (int i =0; i < nums.size (); i++)
-      {
-	Rs -= nums[i];
-	    if (Ls == Rs)
-	        return i;
-    	Ls += nums[i];
-      }
-    return -1;
-  }
+class Solution {
+public:
+    int pivotIndex (vector<int> &nums) {
+
+        int Rs = 0; // Very nice variable name!
+        int Ls = 0; // The same here!
+        for (int number: nums) // Absolutely right way to write for loop! You can also use accumulate() function.
+        {
+            Rs += number;
+        }
+
+        for (int i = 0; i < nums.size (); i++) { //pretty nice way!
+            Rs -= nums[i];
+            if (Ls == Rs)
+                return i;
+            Ls += nums[i];
+        }
+        return -1; // you can also use bool variable and only ONE return statement in the end of the function.
+    }
 };
-int main(){
+
+int main () {
 }
