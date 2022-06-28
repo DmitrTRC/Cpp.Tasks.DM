88c4514e514c0924998b22dbb21b09f8e60c9e9c
Dev Spain <9984398@gmail.com>
2022-06-28 21:51:45 +0300

    Need fix.

--- Common_Tasks/LeetCode/IsomorphicStrings/main.cpp
+++ Common_Tasks/LeetCode/IsomorphicStrings/main.cpp
@@ -2,18 +2,17 @@
 // Created by Dmitry Morozov on 28/6/22.
 //
 #include <unordered_map>
-#include <string)
+#include <string>
+#include <vector>
+#include <iostream>
+
 
 
 class Solution {
 public:
-    void printMap (unordered_map<string, int> &map) {
-        for (auto i: map)
-            cout << i.first << " " << i.second << endl;
-    }
 
-    bool compareMapsValues (std::unordered_map<char, std::array<int>> &map1,
-                            std::unordered_map<char, std::array<int>> &map2) {
+    bool compareMapsValues (std::unordered_map<char, std::vector<int>> &map1,
+                            std::unordered_map<char, std::vector<int>> &map2) {
         auto it_1 = map1.begin ();
         auto it_2 = map2.begin ();
         while (it_1 != map1.end () && it_2 != map2.end ()) {
@@ -26,14 +25,25 @@ public:
         return true;
     }
 
-    bool isIsomorphic (string s, string t) {
-        std::unordered_map<char, std::array<int>> map_s, map_t;
-        for (int i = 0; strlen (s) < lenght (s); ++strlen (s)) {
-            map_s[s[i]].push_back (i);
-            map_t[t[i]].push_back (i);
+    bool isIsomorphic (std::string s, std::string t) {
+        std::vector<char> s_chars =s.begin (), t_chars = t.begin ();
+
+
+        std::unordered_map<char, std::vector<int>> map_s, map_t;
+        for (auto it = s.begin (); it != s.end (); it++) {
+            map_s[s[it]].push_back (it);
+            map_t[t[it]].push_back (it);
         }
 
         return compareMapsValues (map_s, map_t);
     }
 };
+int main(){
+    Solution sol;
+    std::string s = "abb";
+    std::string t = "acc";
+    std::cout << sol.isIsomorphic (s, t) << std::endl;
+    return 0;
+}
+
 
