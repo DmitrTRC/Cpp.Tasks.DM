e915bcd45bbecee5a36b0e5979c2e25c862df475
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-07-01 13:46:55 +0300

    Update main.cpp

--- Common_Tasks/LeetCode/isSubsequence/main.cpp
+++ Common_Tasks/LeetCode/isSubsequence/main.cpp
@@ -1,10 +1,25 @@
 //
 // Created by Dmitry Morozov on 29/6/22.
 //
-
+#include <string>
+#include <map>
+#include <iostream>
+#include <set>
+#include <cassert>
+#include <vector>
+#include <algorithm>
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-
+bool isSubsequence(std::string s, std::string t) {
+int j=0;
+int i=0;
+while (i<s.size()&& j<t.size()){
+    if (s[i]==t[j]){
+        i+=1;
     }
-};
+    j+=1;
+}
+if (i==s.size())
+return true;
+else return false;
+    
